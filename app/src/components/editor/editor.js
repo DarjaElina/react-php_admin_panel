1198e4eae01267c85822a68b915091e4e15c6231
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2023-06-24 11:44:09 +0300

    editor.js modified, helpers folder with iframeLoader.js created

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,10 +1,11 @@
+import "../../helpers/iframeLoader.js";
 import axios from "axios";
 import React, {Component} from 'react';
 
 export default class Editor extends Component {
     constructor() {
         super();
-
+        this.currentPage = "index.html";
         this.state = {
             pageList: [],
             newPageName: ""
@@ -13,8 +14,21 @@ export default class Editor extends Component {
     }
 
     componentDidMount() {
+        this.init(this.currentPage);
+    }
+
+    init(page) {
+        this.iframe = document.querySelector('iframe');
+        this.open(page)
         this.loadPageList();
     }
+
+    open(page) {
+        this.currentPage = `../${page}`;
+        this.iframe.load(this.currentPage, () => {
+            console.log(this.currentPage)
+        });
+   }
 
     loadPageList() {
         axios
@@ -37,26 +51,27 @@ export default class Editor extends Component {
     }
 
     render() {
-        const {pageList} = this.state;
+        // const {pageList} = this.state;
 
-        const pages = pageList.map((page, i) => {
-            return (
-                <h1 key={i}>{page}
-                <a
-                href="#"
-                onClick={() => this.deletePage(page)}>(x)</a>
-                </h1>
-            )
-        });
+        // const pages = pageList.map((page, i) => {
+        //     return (
+        //         <h1 key={i}>{page}
+        //         <a
+        //         href="#"
+        //         onClick={() => this.deletePage(page)}>(x)</a>
+        //         </h1>
+        //     )
+        // });
 
         return (
-            <>
-                <input
-                 onChange={(e) => {this.setState({newPageName: e.target.value})}}
-                 type="text"/>
-                <button onClick={this.createNewPage}>Create page</button>
-                {pages}
-            </>
+            <iframe src={this.currentPage} frameBorder="0"></iframe>
+            // <>
+            //     <input
+            //      onChange={(e) => {this.setState({newPageName: e.target.value})}}
+            //      type="text"/>
+            //     <button onClick={this.createNewPage}>Create page</button>
+            //     {pages}
+            // </>
         )
     }
 }
